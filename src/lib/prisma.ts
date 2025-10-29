2aae3169e1c49342bb59f52a57a4ebbf4d289f42
Juan Antonio <juananthonio23@gmail.com>
2025-10-30 01:35:26 +0700

    refactor: add transaction options with timeout to PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,4 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 export const db = new PrismaClient({
   log: ['error', 'warn'],
+  transactionOptions: {
+    timeout: 1000 * 60 * 1, // 1 minute
+  },
 })
